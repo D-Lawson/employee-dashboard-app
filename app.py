2e209973917feb2f67253117b5d5b886aca8eef7
D-Lawson <dan.lawson@live.co.uk>
2021-11-21 21:58:25 +0000

    Install pymongo, dnspython.  Connect to MongoDB

--- app.py
+++ app.py
@@ -2,16 +2,25 @@ import os
 from flask import (
     Flask, flash, render_template,
     redirect, request, session, url_for)
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 if os.path.exists("env.py"):
     import env
 
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+mongo = PyMongo(app)
 
 @app.route("/")
+@app.route("/get_users")
 def initial():
-    return render_template("base.html")
+    users = mongo.db.users.find()
+    return render_template("users.html", users=users)
 
 
 if __name__ == "__main__":
