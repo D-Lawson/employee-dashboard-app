6a6b30196d8a8377283d98f3becad8e3d6b873d6
D-Lawson <dan.lawson@live.co.uk>
2021-11-23 02:03:41 +0000

    new user tbase via regi.. Ad logout function

--- app.py
+++ app.py
@@ -30,7 +30,35 @@ def login():
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
+    if request.method == "POST":
+        check_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if check_user:
+            flash("This username is already registered")
+            return redirect(url_for("register"))
+
+        create_user = {
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+        mongo.db.users.insert_one(create_user)
+
+        session["user"] = request.form.get("username").lower()
+        flash("Thank you for registering.  Welcome to your personal dashboard.")
+        return redirect(url_for("dashboard", username=session["user"]))
+
     return render_template("register.html")
+
+@app.route("/logout")
+def logout():
+    flash("Sucessfully logged out from dashboard")
+    session.pop("user")
+    return redirect(url_for("login"))
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
 
 
 if __name__ == "__main__":
