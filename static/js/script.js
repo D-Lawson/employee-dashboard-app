8d3e66ead54f522077db6b0f1dee3db07836e6a4
D-Lawson <dan.lawson@live.co.uk>
2021-11-27 02:59:08 +0000

    Adjust css for activities list

--- static/js/script.js
+++ static/js/script.js
@@ -16,17 +16,18 @@ $(document).ready(function () {
     });
     });
 
+/*
+    Conditional formatting for due in 1 week, due in 2 weeks
+*/
 
-number = $('.hide').length
+number = $('.hide').length;
 for (i = 0; i < number; i++) {
 
 
-    var dateid = `date-target-${[i+1]}`
+    var dateid = `date-target-${[i+1]}`;
     var date = document.getElementById(dateid).innerHTML;
     var varDate = new Date(date);
     var today = new Date();
-
-    console.log(date)
 
     Date.prototype.addDays = function(days) {
         var date = new Date(this.valueOf());
@@ -34,14 +35,23 @@ for (i = 0; i < number; i++) {
         return date;
     }
 
-    console.log(varDate)
-    console.log(today)
-
     if(varDate < today.addDays(7)){
         document.getElementById(`conditional-bg-${[i+1]}`).style.color = "red";
         } else if (varDate < today.addDays(14)) {
             document.getElementById(`conditional-bg-${[i+1]}`).style.color = "orange";
         }
-
 }
 
+/*
+    Test for responsive text
+*/
+
+length1 = $('.hide').length
+for (i = 0; i < length1; i++) {
+
+    get_length = document.getElementsByClassName(`length-size-${[i+1]}`).innerHTML.length;
+
+    if((get_length > 30) && ($(window).width() < 992)){
+        document.getElementsByClassName(`length-size-${[i+1]}`).style.fontSize = "x-small";
+        } 
+}
